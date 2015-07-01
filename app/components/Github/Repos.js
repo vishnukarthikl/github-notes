d109a8bc00046d13114487ffdb1579c87c368592
Vishnu karthik <vishnucarbon@gmail.com>
2015-07-01 18:43:57 +0530

    showing github data

--- app/components/Github/Repos.js
+++ app/components/Github/Repos.js
@@ -2,10 +2,20 @@ var React = require('react');
 
 var Repos = React.createClass({
         render: function () {
+            var repos = this.props.repos.map(function (repo, index) {
+                return <li className="list-group-item">
+                {repo.html_url && <h4>
+                    <a href={repo.html_url}>{repo.name}</a>
+                </h4>}
+                {repo.description && <p> {repo.description} </p> }
+                </li>
+            });
             return (
-              <div> Repos
-              {this.props.repos}
-              </div>
+                <div>
+                    <h3> Repos </h3>
+                    <ul className="list-group">{repos} </ul>
+                </div>
+
             );
         }
     }
